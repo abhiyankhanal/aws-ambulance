f04dfd9f62ff4754409f388dde85d9ccaf0ccac8
Manjil Nepali <manjilnepali98@gmail.com>
2024-04-06 18:57:15 +0545

    tst

--- packages/build/index.js
+++ packages/build/index.js
@@ -63,11 +63,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var AWS = __importStar(require("aws-sdk"));
-var monitorResourceType = ['S3Bucket'];
+var child_process_1 = require("child_process");
+var monitorResourceType = ["S3Bucket"];
 var commander_1 = require("commander");
 var inquirer_1 = __importDefault(require("inquirer"));
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-inquirer_1.default.registerPrompt('table', require('inquirer-table-prompt'));
+inquirer_1.default.registerPrompt("table", require("inquirer-table-prompt"));
 function getDetectorId() {
     return __awaiter(this, void 0, void 0, function () {
         var config, guardduty, detectors, err_1;
@@ -75,7 +76,7 @@ function getDetectorId() {
             switch (_a.label) {
                 case 0:
                     config = {
-                        region: 'us-east-1',
+                        region: "us-east-1",
                     };
                     guardduty = new AWS.GuardDuty(config);
                     _a.label = 1;
@@ -88,12 +89,12 @@ function getDetectorId() {
                         return [2 /*return*/, detectors.DetectorIds[0]]; // Assuming only one detector is present
                     }
                     else {
-                        throw new Error('No detectors found.');
+                        throw new Error("No detectors found.");
                     }
                     return [3 /*break*/, 4];
                 case 3:
                     err_1 = _a.sent();
-                    console.error('Error getting detector ID:', err_1);
+                    console.error("Error getting detector ID:", err_1);
                     return [2 /*return*/, null];
                 case 4: return [2 /*return*/];
             }
@@ -102,7 +103,7 @@ function getDetectorId() {
 }
 function getVulnerableServices() {
     return __awaiter(this, void 0, void 0, function () {
-        var detectorId, config, guardduty, findings, vulnerableServices_1, findingIds, promises, findingDetailsArray, err_2;
+        var detectorId, config, guardduty, totalFindingList, first_findings, second_findings, third_findings, vulnerableServices_1, findingIds, promises, findingDetailsArray, err_2;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0: return [4 /*yield*/, getDetectorId()];
@@ -112,26 +113,51 @@ function getVulnerableServices() {
                         return [2 /*return*/, []];
                     }
                     config = {
-                        region: 'us-east-1', // Replace 'your-aws-region' with your AWS region code
+                        region: "us-east-1", // Replace 'your-aws-region' with your AWS region code
                     };
                     guardduty = new AWS.GuardDuty(config);
                     _a.label = 2;
                 case 2:
-                    _a.trys.push([2, 5, , 6]);
+                    _a.trys.push([2, 7, , 8]);
+                    totalFindingList = [];
                     return [4 /*yield*/, guardduty
-                            .listFindings({ DetectorId: detectorId, MaxResults: 20 })
+                            .listFindings({
+                            DetectorId: detectorId,
+                            MaxResults: 50,
+                        })
                             .promise()];
                 case 3:
-                    findings = _a.sent();
+                    first_findings = _a.sent();
+                    totalFindingList.push.apply(totalFindingList, ((first_findings === null || first_findings === void 0 ? void 0 : first_findings.FindingIds) || []));
+                    return [4 /*yield*/, guardduty
+                            .listFindings({
+                            DetectorId: detectorId,
+                            MaxResults: 50,
+                            NextToken: first_findings.NextToken,
+                        })
+                            .promise()];
+                case 4:
+                    second_findings = _a.sent();
+                    totalFindingList.push.apply(totalFindingList, ((second_findings === null || second_findings === void 0 ? void 0 : second_findings.FindingIds) || []));
+                    return [4 /*yield*/, guardduty
+                            .listFindings({
+                            DetectorId: detectorId,
+                            MaxResults: 50,
+                            NextToken: second_findings.NextToken,
+                        })
+                            .promise()];
+                case 5:
+                    third_findings = _a.sent();
+                    totalFindingList.push.apply(totalFindingList, ((third_findings === null || third_findings === void 0 ? void 0 : third_findings.FindingIds) || []));
                     vulnerableServices_1 = [];
-                    findingIds = findings.FindingIds || [];
+                    findingIds = totalFindingList || [];
                     promises = findingIds.map(function (findingId) {
                         return guardduty
                             .getFindings({ DetectorId: detectorId, FindingIds: [findingId] })
                             .promise();
                     });
                     return [4 /*yield*/, Promise.all(promises)];
-                case 4:
+                case 6:
                     findingDetailsArray = _a.sent();
                     findingDetailsArray.forEach(function (findingDetails) {
                         var finding = findingDetails.Findings && findingDetails.Findings[0];
@@ -143,14 +169,24 @@ function getVulnerableServices() {
                         }
                     });
                     return [2 /*return*/, vulnerableServices_1];
-                case 5:
+                case 7:
                     err_2 = _a.sent();
-                    console.error('Error:', err_2);
+                    console.error("Error:", err_2);
                     return [2 /*return*/, []];
-                case 6: return [2 /*return*/];
+                case 8: return [2 /*return*/];
             }
         });
     });
+}
+function showLoadingUI() {
+    var animation = ["|", "/", "-", "\\"]; // Array of characters for the loading animation
+    var i = 0;
+    var interval = setInterval(function () {
+        process.stdout.write("\r" + "Fetching AWS S3 Logs " + animation[i]); // Overwrite the previous line
+        i = (i + 1) % animation.length; // Increment the index and wrap around if needed
+    }, 100); // Change the animation speed as needed
+    // Return the interval object so it can be cleared later
+    return interval;
 }
 function getListOfServices() {
     return __awaiter(this, void 0, void 0, function () {
@@ -170,8 +206,12 @@ function getListOfServices() {
                         });
                         return test.push({
                             title: item.Title,
+                            description: item.Description,
                             type: item.Type,
                             services: test12,
+                            severity: item.Severity,
+                            createdAt: item.CreatedAt,
+                            region: item.Region,
                         });
                     });
                     return [2 /*return*/, test];
@@ -180,52 +220,33 @@ function getListOfServices() {
     });
 }
 var program = new commander_1.Command();
-function renderOptions() {
-    return __awaiter(this, void 0, void 0, function () {
-        var questions, answers;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    questions = [
-                        {
-                            type: 'list',
-                            name: 'option',
-                            message: 'Select an option:',
-                            choices: ['Option 1', 'Option 2', 'Option 3'],
-                        },
-                    ];
-                    return [4 /*yield*/, inquirer_1.default.prompt(questions)];
-                case 1:
-                    answers = _a.sent();
-                    return [2 /*return*/];
-            }
-        });
-    });
-}
 program
-    .version('1.0.0')
-    .description('A simple CLI program')
-    .option('-l, --list <message>', 'Set the greeting message', 'Hello')
+    .version("1.0.0")
+    .description("A simple CLI program")
+    .option("-l, --list <message>", "Set the greeting message", "Hello")
     .action(function () { return __awaiter(void 0, void 0, void 0, function () {
-    var list, s3Alerts, answer;
+    var loadingInterval, list, s3Alerts, answer, awsCommand;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, getListOfServices()];
+            case 0:
+                loadingInterval = showLoadingUI();
+                return [4 /*yield*/, getListOfServices()];
             case 1:
                 list = _a.sent();
+                clearInterval(loadingInterval); // Stop the loading animation
                 s3Alerts = list.map(function (item, index) { return ({
                     value: index,
-                    name: "[".concat(item.type, "]: ").concat(item.title),
+                    name: "[".concat(item.type, "]: \n Bucket ARN: ").concat(item.services[0].arn, " \n Title: ").concat(item.title, "\n Region: ").concat(item.region, "\n Severity: ").concat(item.severity, "\n Date: ").concat(item.createdAt, "\n"),
                 }); });
                 return [4 /*yield*/, inquirer_1.default.prompt([
                         {
-                            type: 'table',
-                            name: 'Action Plan',
-                            message: 'Select item to resolve',
+                            type: "table",
+                            name: "Action Plan",
+                            message: "Select item to resolve",
                             columns: [
                                 {
-                                    name: 'Action',
-                                    value: 'action',
+                                    name: "Action",
+                                    value: "action",
                                 },
                             ],
                             rows: s3Alerts,
@@ -233,7 +254,20 @@ program
                     ])];
             case 2:
                 answer = _a.sent();
-                console.log(answer);
+                //locking AWS S3 service
+                console.log("🚀 Initiating lock process for S3 bucket");
+                console.log("🔒 Initiating lock process for S3 bucket ...");
+                console.log("🔍 Checking bucket status...");
+                console.log("🔒 Locking bucket to prevent modifications...");
+                console.log("⏳ Locking process in progress...");
+                awsCommand = "aws stepfunctions start-execution --state-machine-arn arn:aws:states:us-east-1:684378237653:stateMachine:AWSAmbulanceStartExecution-xdYEynYSiTX3 --input '{\"arns\": [\"arn:aws:s3:::testing-aws-ambulance-s3\", \"arn:aws:lambda:us-east-1:684378237653:function:testing-aws-ambulance-lambda\"]}'";
+                (0, child_process_1.exec)(awsCommand, function (error, stdout, stderr) {
+                    if (error) {
+                        console.log("Command output: ".concat(stdout));
+                        return;
+                    }
+                    console.log("🔒 S3 bucket is now locked successfully. Email will be send once lockdown is completed.");
+                });
                 return [2 /*return*/];
         }
     });
--- packages/src/index.ts
+++ packages/src/index.ts
@@ -40,12 +40,38 @@ async function getVulnerableServices(): Promise<AWS.GuardDuty.Finding[]> {
 
   const guardduty = new AWS.GuardDuty(config);
   try {
-    const findings = await guardduty
-      .listFindings({ DetectorId: detectorId, MaxResults: 20 })
+    const totalFindingList = [];
+    const first_findings = await guardduty
+      .listFindings({
+        DetectorId: detectorId,
+        MaxResults: 50,
+      })
       .promise();
 
+    totalFindingList.push(...(first_findings?.FindingIds || []));
+
+    const second_findings = await guardduty
+      .listFindings({
+        DetectorId: detectorId,
+        MaxResults: 50,
+        NextToken: first_findings.NextToken,
+      })
+      .promise();
+
+    totalFindingList.push(...(second_findings?.FindingIds || []));
+
+    const third_findings = await guardduty
+      .listFindings({
+        DetectorId: detectorId,
+        MaxResults: 50,
+        NextToken: second_findings.NextToken,
+      })
+      .promise();
+
+    totalFindingList.push(...(third_findings?.FindingIds || []));
+
     const vulnerableServices: AWS.GuardDuty.Finding[] = [];
-    const findingIds = findings.FindingIds || [];
+    const findingIds = totalFindingList || [];
     const promises = findingIds.map((findingId) =>
       guardduty
         .getFindings({ DetectorId: detectorId, FindingIds: [findingId] })
@@ -71,6 +97,19 @@ async function getVulnerableServices(): Promise<AWS.GuardDuty.Finding[]> {
   }
 }
 
+function showLoadingUI() {
+  const animation = ["|", "/", "-", "\\"]; // Array of characters for the loading animation
+  let i = 0;
+
+  const interval = setInterval(() => {
+    process.stdout.write("\r" + "Fetching AWS S3 Logs " + animation[i]); // Overwrite the previous line
+    i = (i + 1) % animation.length; // Increment the index and wrap around if needed
+  }, 100); // Change the animation speed as needed
+
+  // Return the interval object so it can be cleared later
+  return interval;
+}
+
 async function getListOfServices() {
   const vulnerableServices = await getVulnerableServices();
 
@@ -83,8 +122,12 @@ async function getListOfServices() {
     });
     return test.push({
       title: item.Title,
+      description: item.Description,
       type: item.Type,
       services: test12,
+      severity: item.Severity,
+      createdAt: item.CreatedAt,
+      region: item.Region,
     });
   });
 
@@ -93,29 +136,18 @@ async function getListOfServices() {
 
 const program = new Command();
 
-async function renderOptions() {
-  const questions = [
-    {
-      type: "list",
-      name: "option",
-      message: "Select an option:",
-      choices: ["Option 1", "Option 2", "Option 3"],
-    },
-  ];
-
-  const answers = await inquirer.prompt(questions);
-}
-
 program
   .version("1.0.0")
   .description("A simple CLI program")
   .option("-l, --list <message>", "Set the greeting message", "Hello")
   .action(async () => {
+    const loadingInterval = showLoadingUI();
     const list = await getListOfServices();
+    clearInterval(loadingInterval); // Stop the loading animation
 
     const s3Alerts = list.map((item, index) => ({
       value: index,
-      name: `[${item.type}]: ${item.title}`,
+      name: `[${item.type}]: \n Bucket ARN: ${item.services[0].arn} \n Title: ${item.title}\n Region: ${item.region}\n Severity: ${item.severity}\n Date: ${item.createdAt}\n`,
     }));
 
     // Define an array of options to render as a table
